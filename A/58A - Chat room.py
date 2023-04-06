a6c95583b1d515c9e334e9295ba1686702ead46c
Odilbek99 <umaralievodili@gmail.com>
2023-04-06 22:18:55 +0300

    Chat Room

--- A/58A - Chat room.py
+++ A/58A - Chat room.py
@@ -1,10 +1,19 @@
 str1 = input()
 
 msg = 'hello'
-res = ''
+
 c = 0
-for i in range(len(str1)):
-    if str1[i] == msg[c]:
-        c+=1
+res = ''
+for char in msg:
+    for i in range(c,len(str1)):
+        c += 1
+        if char == str1[i]:
+            res += str1[i]
+            break
+if res == msg:
+    print('YES')
+else:
+    print('NO')
     
-print(c)
+        
+
