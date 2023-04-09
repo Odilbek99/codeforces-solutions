c5c4a280550c9c4ce7d0febc71a69d9bc6dcadbf
Odilbek99 <umaralievodili@gmail.com>
2023-04-09 16:16:14 +0300

    580A

--- A/580A - Kefa and First Steps.py
+++ A/580A - Kefa and First Steps.py
@@ -2,14 +2,16 @@ n = int(input())
 line = list(map(int,input().split()))
 
 c = 1
-max_ = 0
+max_ = 1
 for i in range(len(line)-1):
-    
-    if line[i] <= line[i+1]:
-        print(line[i],line[i+1])
+    if max_ == n:
+        print(n)
+        break
+    elif line[i] <= line[i+1]:
+        # print(line[i],line[i+1])
         c+=1
-    else:
         max_ = max(c,max_)
+    else:
         c=1
 
 print(max_)
